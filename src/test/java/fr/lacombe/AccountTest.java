e0b53990706c57ad724004aaf9f1e9e9f7b7a421
alexandre pouget <pouget.al@gmail.com>
2018-02-21 16:32:15 +0100

    Give empty bank statement

--- src/test/java/fr/lacombe/AccountTest.java
+++ src/test/java/fr/lacombe/AccountTest.java
@@ -10,10 +10,11 @@ import static org.junit.Assert.*;
 public class AccountTest {
 
     @Test
-    public void printEmptyDeposit() {
+    public void printEmptyStatement() {
         Account account = new Account();
         BankStatement expectedBankStatement = new BankStatement("date || credit || debit || balance");
         Assertions.assertThat(account.printStatement()).isEqualTo(expectedBankStatement);
-
     }
+
+
 }
